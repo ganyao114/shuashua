dddd26190e96737a8aa7d4ad59e1e08366c43bc2
ganyao114 <939543405@qq.com>
2016-08-01 11:13:19 +0800

    shuashua

---
+++ app/src/main/java/com/shuashua/buss/View/Activity/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.shuashua.buss.View.Activity;
+
+import net.gy.SwiftFrameWork.MVP.View.context.activity.BaseAppCompactActivity;
+
+/**
+ * Created by pc on 16/8/1.
+ */
+public class BaseActivity extends BaseAppCompactActivity{
+}
