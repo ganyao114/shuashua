d5914285a6a589d6b60cba19584cf79457861444
ganyao114 <939543405@qq.com>
2016-09-06 10:05:15 +0800

    cache update

---
+++ app/src/main/java/com/shuashua/buss/View/Window/StaffMngPopupWindow.java
@@ -0,0 +1,59 @@
+package com.shuashua.buss.View.Window;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.PopupWindow;
+
+import com.shuashua.buss.R;
+
+public class StaffMngPopupWindow extends PopupWindow {
+
+	private Button  delbtn, cancelBtn;
+	private View mMenuView;
+
+	@SuppressLint("InflateParams")
+	public StaffMngPopupWindow(Context context, OnClickListener itemsOnClick) {
+		super(context);
+		LayoutInflater inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		mMenuView = inflater.inflate(R.layout.layout_dialog_staffmng, null);
+		delbtn = (Button) mMenuView.findViewById(R.id.del_staff);
+		cancelBtn = (Button) mMenuView.findViewById(R.id.staff_cancel);
+		cancelBtn.setOnClickListener(itemsOnClick);
+		delbtn.setOnClickListener(itemsOnClick);
+
+		this.setContentView(mMenuView);
+		this.setWidth(LayoutParams.MATCH_PARENT);
+		this.setHeight(LayoutParams.WRAP_CONTENT);
+		this.setFocusable(true);
+		this.setAnimationStyle(R.style.PopupAnimation);
+		ColorDrawable dw = new ColorDrawable(0x80000000);
+		this.setBackgroundDrawable(dw);
+		mMenuView.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			@SuppressLint("ClickableViewAccessibility")
+			public boolean onTouch(View v, MotionEvent event) {
+
+				int height = mMenuView.findViewById(R.id.pop_layout).getTop();
+				int y = (int) event.getY();
+				if (event.getAction() == MotionEvent.ACTION_UP) {
+					if (y < height) {
+						dismiss();
+					}
+				}
+				return true;
+			}
+		});
+
+	}
+
+}
